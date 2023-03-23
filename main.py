5f8fa253be879922034f6e427df18e69810c9c2d
Patrick <pc799@bath.ac.uk>
2023-03-23 22:11:59 +0000

    Split classes into files

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-class Player():
-    def __init__(self, name, bidItem, bidPrice):
-        self.name = name
-        self.bidItem = bidItem
-        self.bidPrice = bidPrice
-        self.bidIndex = []
-
-    def createBidIndex(self):
-        self.bidIndex = list(zip(self.bidItem, self.bidPrice))
+from Player import *
 
 player1 = Player("Patrick", [0, 1], [6, 0])
-
+player1.createBidIndex()
+print(player1.bidIndex)
